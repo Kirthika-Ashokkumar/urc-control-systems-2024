d139ccbaf127a0fc953c281b0d1ef03d57c5fc9c
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2025-07-03 14:54:25 -0700

    implment some drivetrain math

--- drive/include/drivetrain_math.hpp
+++ drive/include/drivetrain_math.hpp
@@ -1,18 +1,40 @@
 #include "swerve_module.hpp"
 #include "vector2d.hpp"
+#include <array>
 #include <cmath>
+#include <cstdlib>
 #include <libhal/units.hpp>
-#include <span>
+#include <numbers>
+#include <sys/types.h>
 namespace sjsu::drive {
 
+using namespace std::chrono_literals;
+
 // chassis speed to states
-void chassis_velocities_to_module_vectors(
+std::array<vector2d, module_count> chassis_velocities_to_module_vectors(
   chassis_velocities p_chassis_velocities,
-  std::span<swerve_module> p_modules,
-  std::span<vector2d> p_vector_buffer);
+  std::array<swerve_module, module_count> p_modules)
+{
+  std::array<vector2d, module_count> vectors;
+  //  convert rotaiton speed to radians
+  float rotational_vel_radians_per_sec =
+    p_chassis_velocities.rotational_vel * std::numbers::pi / 180;
+  for (uint i = 0; i < vectors.size(); i++) {
+    // translation vector is the same
+    vector2d transition = p_chassis_velocities.translation;
+    // rotation position vector by 90 degrees is the vector for 1 rad per sec
+    vector2d rotation = rotational_vel_radians_per_sec *
+                        vector2d::rotate_90_ccw(p_modules[i].settings.position);
+
+    vectors[i] = transition + rotation;
+  }
+  return vectors;
+}
+
 // module state validity score (0 is complete match)
-float module_validity_strain_score(std::span<swerve_module> p_modules,
-                                   std::span<vector2d> p_vectors);
+float module_validity_strain_score(
+  std::array<swerve_module, module_count> p_modules,
+  std::array<vector2d, module_count> p_vectors);
 // {
 //     vector2d transition(0,0);
 //     for (auto& v : p_vectors) {
@@ -31,23 +53,91 @@ swerve_module_state calculate_closest_state(swerve_module p_module,
 // calc interpolation time (single module and/or all)
 hal::time_duration calculate_total_interpolation_time(
   swerve_module p_module,
-  swerve_module_state p_end_state);
+  swerve_module_state p_end_state)
+{
+  // using a relatively primitive calculation for now
+
+  // calc speed interpolation time
+  meters_per_sec speed_diff =
+    fabsf(p_module.get_actual_state_cache().propulsion_velocity -
+          p_end_state.propulsion_velocity);
+
+  hal::time_duration propulsion_transition_time =
+    1s;  // TODO: make more readable/make 1 line?
+  propulsion_transition_time *= speed_diff / p_module.settings.acceleration;
+
+  // hal::time_duration test = 1s * (speed_diff /
+  // p_module.settings.max_acceleration);
+  hal::degrees angle_diff = fabsf(
+    p_module.get_actual_state_cache().steer_angle - p_end_state.steer_angle);
+  hal::time_duration steer_transition_time = 1s;
+  steer_transition_time *= angle_diff / p_module.settings.turn_speed;
+
+  if (propulsion_transition_time > steer_transition_time) {
+    return propulsion_transition_time;
+  } else {
+    return steer_transition_time;
+  }
+}
+
 hal::time_duration calculate_total_interpolation_time(
-  std::span<swerve_module> p_modules,
-  std::span<swerve_module_state> p_end_states);
+  std::array<swerve_module, module_count> p_modules,
+  std::array<swerve_module_state, module_count> p_end_states)
+{
+  hal::time_duration max_time = 0s;
+  for (uint i = 0; i < p_modules.size(); i++) {
+    hal::time_duration time =
+      calculate_total_interpolation_time(p_modules[i], p_end_states[i]);
+    if (time > max_time) {
+      max_time = time;
+    }
+  }
+  return max_time;
+}
 
 // detect interpolation conflicts (ivalid interpolation how calc?)
 
 // scale down speeds
-void scale_down_propulsion_speed(std::span<swerve_module> p_modules,
-                                 std::span<swerve_module_state> p_states);
+std::array<swerve_module_state, module_count> scale_down_propulsion_speed(
+  std::array<swerve_module, module_count> p_modules,
+  std::array<swerve_module_state, module_count> p_states)
+{
+  float percent = 1.0;
+  for (uint i=0; i < p_modules.size(); i++) {
+    if(fabsf(p_states[i].propulsion_velocity*percent) > p_modules[i].settings.max_speed) {
+      percent = p_modules[i].settings.max_speed / fabsf(p_states[i].propulsion_velocity);
+    }
+  }
+  std::array<swerve_module_state, module_count> scale_down_states;
+  for (uint i=0; i< scale_down_states.size(); i++) {
+    scale_down_states[i] = {
+      .steer_angle = p_states[i].steer_angle,
+      .propulsion_velocity = p_states[i].propulsion_velocity * percent
+    };
+  }
+  return scale_down_states;
+}
 // interpolate state (by % and/or time) - time can't be done with just 2 states
-// since we would have to sync all the modules 
+// since we would have to sync all the modules
 // interpolate states (by % and/ortime) - why would
 // percent be a thing?
-swerve_module_state interpolate_state(float p_percent, swerve_module_state p_start_state, swerve_module_state p_end_state);
-void interpolate_states(hal::time_duration p_cycle_time,
-                        std::span<swerve_module> p_modules,
-                        std::span<swerve_module_state> p_end_states,
-  std::span<swerve_module_state> p_target_states_buffer);
+swerve_module_state interpolate_state(float p_percent,
+                                      swerve_module_state p_start_state,
+                                      swerve_module_state p_end_state);
+std::array<swerve_module_state, module_count> interpolate_states(
+  hal::time_duration p_cycle_time,
+  std::array<swerve_module, module_count> p_modules,
+  std::array<swerve_module_state, module_count> p_end_states)
+{
+  std::array<swerve_module_state, module_count> interpolated_states;
+  float percent =
+    p_cycle_time /
+    calculate_total_interpolation_time(
+      p_modules, p_end_states);  // TODO: check this is not integer division
+  for (uint i = 0; i < p_modules.size(); i++) {
+    interpolated_states[i] = interpolate_state(
+      percent, p_modules[i].get_actual_state_cache(), p_end_states[i]);
+  }
+  return interpolated_states;
+}
 }  // namespace sjsu::drive
--- drive/include/swerve_module.hpp
+++ drive/include/swerve_module.hpp
@@ -1,43 +1,57 @@
 #pragma once
 
 #include "swerve_structs.hpp"
+#include "vector2d.hpp"
 #include <libhal/servo.hpp>
 #include <libhal/units.hpp>
 
 namespace sjsu::drive {
-class swerve_module {
-    // vars:
-    // steer postion read & control (maybe also contains restrictions) (has homing function)
-    // prop vel read & control (maybe also contrans restrictions)
-    // settings (positions & maybe constraints of motors if not included)
-    // most recent measurements (so it don't send a read command every time)
 
-    // methods:
-    // calc target state for vector (closest angle for valid state)
-    vector2d calc_module_vector(chassis_velocities p_chassis_velocities);
-    // set target state
-    void set_target_state(swerve_module_state p_target_state);
-    // TODO: move to calc file?
-    swerve_module_state calc_closest_state(vector2d p_module_vector);
-    // optimize freedom
-    swerve_module_state calc_freest_state(vector2d p_module_vector);
-    bool can_reach_state(swerve_module_state p_state);
-    
-    // time for transition
-    hal::time_duration transition_time(swerve_module_state p_end_state);
-
-    //reads state cache
-    swerve_module_state get_actual_state_cache();
-    // refresh state cache
-    swerve_module_state refresh_actual_state_cache();
-
-    // get actual & target state
-    // get actual angle
-    // get actual prop vel
-    // get target angle
-    // get target prop vel
-    // set target angle
-    // set target prop vel
-
+struct swerve_module_settings
+{
+  vector2d position;
+  meters_per_sec max_speed;
+  meters_per_sec_per_sec acceleration;
+  deg_per_sec turn_speed;
+  hal::degrees min_angle;
+  hal::degrees max_angle;
 };
-}
+
+class swerve_module
+{
+public:
+  swerve_module_settings settings;
+  // vars:
+  // steer postion read & control (maybe also contains restrictions) (has homing
+  // function) prop vel read & control (maybe also contrans restrictions)
+  // settings (positions & maybe constraints of motors if not included)
+  // most recent measurements (so it don't send a read command every time)
+
+  // methods:
+  // calc target state for vector (closest angle for valid state)
+  vector2d calc_module_vector(chassis_velocities p_chassis_velocities);
+  // set target state
+  void set_target_state(swerve_module_state p_target_state);
+  // TODO: move to calc file?
+  swerve_module_state calc_closest_state(vector2d p_module_vector);
+  // optimize freedom
+  swerve_module_state calc_freest_state(vector2d p_module_vector);
+  bool can_reach_state(swerve_module_state p_state);
+
+  // time for transition
+  hal::time_duration transition_time(swerve_module_state p_end_state);
+
+  // reads state cache
+  swerve_module_state get_actual_state_cache();
+  // refresh state cache
+  swerve_module_state refresh_actual_state_cache();
+
+  // get actual & target state
+  // get actual angle
+  // get actual prop vel
+  // get target angle
+  // get target prop vel
+  // set target angle
+  // set target prop vel
+};
+}  // namespace sjsu::drive
